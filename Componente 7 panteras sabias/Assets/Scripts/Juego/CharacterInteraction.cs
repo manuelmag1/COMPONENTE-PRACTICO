156d3328dd88e33971582b5398d7f828d974dda2
manuelmag1 <maglioni124@hotmail.com>
2026-04-11 21:40:25 +0200

    JUEGO FULL

--- Componente 7 panteras sabias/Assets/Scripts/Juego/CharacterInteraction.cs
+++ Componente 7 panteras sabias/Assets/Scripts/Juego/CharacterInteraction.cs
@@ -4,22 +4,36 @@ using System.Collections;
 public class InteraccionPersonaje : MonoBehaviour
 {
     private Animator anim;
-    private bool yaFueGolpeado = false;
+    public bool yaFueGolpeado = false;
 
     void Start()
     {
-        // Como el script ahora está en Mario, buscamos el Animator aquí mismo
         anim = GetComponent<Animator>();
+
+        // (Opcional) Si pusiste aquí el código para apagar a los que no juegan desde el menú, déjalo aquí:
+        // if (gameObject.name == "Mario" && !SeleccionPersonajes.juegaMario) gameObject.SetActive(false);
+        // if (gameObject.name == "Wario" && !SeleccionPersonajes.juegaWario) gameObject.SetActive(false);
+        // if (gameObject.name == "Peach" && !SeleccionPersonajes.juegaPeach) gameObject.SetActive(false);
+        // if (gameObject.name == "Kong" && !SeleccionPersonajes.juegaKong) gameObject.SetActive(false);
     }
 
-    // Esta función detectará automáticamente si algo toca la cápsula de Mario
     void OnTriggerEnter(Collider otro)
     {
-        // Solo nos importa si lo que nos tocó tiene la etiqueta "Podoboo"
         if (otro.CompareTag("Podoboo") && !yaFueGolpeado)
         {
-            Debug.Log("¡Mario fue quemado por el Podoboo!");
+            // Usamos gameObject.name para saber exactamente quién se quemó
+            Debug.Log("¡" + gameObject.name + " fue quemado por el Podoboo!");
+            
             yaFueGolpeado = true;
+
+            // --- ¡ESTA ES LA LÍNEA NUEVA VITAL! ---
+            // Le avisamos al cerebro del juego que este personaje acaba de perder
+            if (GameManager.Instancia != null) 
+            {
+                GameManager.Instancia.RegistrarMuerte();
+            }
+            // --------------------------------------
+
             StartCoroutine(SecuenciaDeEliminacion());
         }
     }
@@ -34,7 +48,7 @@ public class InteraccionPersonaje : MonoBehaviour
         // Esperamos a que termine la animación
         yield return new WaitForSeconds(1.5f);
 
-        // Desactivamos a Mario
+        // Desactivamos al personaje
         gameObject.SetActive(false);
     }
 }
--- Componente 7 panteras sabias/Assets/Scripts/Juego/GameManager.cs
+++ Componente 7 panteras sabias/Assets/Scripts/Juego/GameManager.cs
@@ -1,53 +1,138 @@
 using UnityEngine;
 using TMPro;
 using System.Collections;
+using UnityEngine.SceneManagement; // ¡NUEVO! Necesario para cambiar de escena
 
 public class GameManager : MonoBehaviour
 {
-    // Esto es un "Singleton". Permite que cualquier otro script encuentre al GameManager fácilmente.
     public static GameManager Instancia; 
 
     [Header("Interfaz")]
     public TextMeshProUGUI textoCuentaRegresiva;
+    public TextMeshProUGUI textoVictoria; 
+    public GameObject panelBotonesFinales; // ¡NUEVO! Aquí arrastraremos los botones
+
+    [Header("Jugadores")]
+    public GameObject[] todosLosJugadores; 
+    private int jugadoresVivos = 0;
 
     [Header("Estado del Juego")]
     public bool juegoActivo = false;
 
     void Awake()
     {
-        // Configuramos el Singleton
         if (Instancia == null) { Instancia = this; }
         else { Destroy(gameObject); }
     }
 
     void Start()
     {
-        // Apenas carga la escena, empezamos la cuenta regresiva
         StartCoroutine(RutinaInicioJuego());
+        StartCoroutine(ContarJugadoresAlInicio());
+    }
+
+    IEnumerator ContarJugadoresAlInicio()
+    {
+        yield return new WaitForEndOfFrame();
+        
+        foreach(GameObject jugador in todosLosJugadores)
+        {
+            if (jugador != null && jugador.activeSelf)
+            {
+                jugadoresVivos++;
+            }
+        }
     }
 
     IEnumerator RutinaInicioJuego()
     {
-        // 1. Bloqueamos el juego por si acaso
         juegoActivo = false; 
         textoCuentaRegresiva.gameObject.SetActive(true);
 
-        // 2. Secuencia de números
         textoCuentaRegresiva.text = "3";
-        yield return new WaitForSeconds(1f); // Esperamos 1 segundo
-
+        yield return new WaitForSeconds(1f);
         textoCuentaRegresiva.text = "2";
         yield return new WaitForSeconds(1f);
-
         textoCuentaRegresiva.text = "1";
         yield return new WaitForSeconds(1f);
 
-        // 3. ¡Empieza la acción!
         textoCuentaRegresiva.text = "YA!";
-        juegoActivo = true; // Al poner esto en true, liberamos la cuerda y los controles
+        juegoActivo = true; 
 
-        // 4. Esperamos un segundo más y borramos el texto
         yield return new WaitForSeconds(1f);
         textoCuentaRegresiva.gameObject.SetActive(false); 
+    }
+
+    public void RegistrarMuerte()
+    {
+        jugadoresVivos--;
+
+        if (jugadoresVivos == 1)
+        {
+            DeclararGanador();
+        }
+        else if (jugadoresVivos <= 0) 
+        {
+            juegoActivo = false;
+            textoVictoria.text = "EMPATE!";
+            textoVictoria.gameObject.SetActive(true);
+            
+            // Si hay empate, también mostramos los botones
+            if (panelBotonesFinales != null) panelBotonesFinales.SetActive(true);
+        }
+    }
+
+    void DeclararGanador()
+    {
+        juegoActivo = false; 
+
+        GameObject[] podoboos = GameObject.FindGameObjectsWithTag("Podoboo");
+        foreach(GameObject fuego in podoboos)
+        {
+            fuego.SetActive(false);
+        }
+
+        GameObject ganador = null;
+        foreach(GameObject jugador in todosLosJugadores)
+        {
+            if (jugador != null && jugador.activeSelf)
+            {
+                InteraccionPersonaje script = jugador.GetComponent<InteraccionPersonaje>();
+                if (script != null && script.yaFueGolpeado == false)
+                {
+                    ganador = jugador; 
+                }
+            }
+        }
+
+        if (ganador != null)
+        {
+            textoVictoria.text = ganador.name.ToUpper() + " WINS!";
+            textoVictoria.gameObject.SetActive(true);
+            
+            // ¡NUEVO! Mostramos los botones de revancha y salir
+            if (panelBotonesFinales != null) panelBotonesFinales.SetActive(true);
+
+            Animator anim = ganador.GetComponent<Animator>();
+            if (anim != null)
+            {
+                anim.SetTrigger("Victoria");
+            }
+        }
+    }
+
+    // --- ¡NUEVAS FUNCIONES PARA LOS BOTONES! ---
+
+    public void IrARevancha()
+    {
+        // Cambia "MenuPrincipal" por el nombre exacto de tu escena de menú.
+        // Las variables estáticas recordarán quiénes estaban jugando.
+        SceneManager.LoadScene(0); 
+    }
+
+    public void SalirDelJuego()
+    {
+        Debug.Log("Cerrando el juego...");
+        Application.Quit(); // Esto cerrará el juego cuando lo exportes a .exe
     }
 }
